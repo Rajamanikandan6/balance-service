03b2e9b50326b334a0808a6eacd41e09be7d3011
Rajamanikandan Soliappan <rajamanikandanso@maveric-systems.com>
2022-08-25 14:20:04 +0530

    added create balance functionality

--- src/main/java/com/maveric/balanceservice/controller/BalanceServiceController.java
+++ src/main/java/com/maveric/balanceservice/controller/BalanceServiceController.java
@@ -1,4 +1,26 @@
 package com.maveric.balanceservice.controller;
 
+import com.maveric.balanceservice.model.Balance;
+import com.maveric.balanceservice.service.BalanceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BalanceServiceController {
+
+    @Autowired
+    BalanceService balanceService;
+
+    @PostMapping("/accounts/{accountId}/balances")
+    public ResponseEntity<Balance> createBalance(@RequestBody Balance balance, @PathVariable String accountId) {
+        balance.setAccountId(accountId);
+        Balance balanceDetails = balanceService.createBalance(balance);
+        return ResponseEntity.status(HttpStatus.CREATED).body(balanceDetails);
+    }
+
 }
--- src/main/java/com/maveric/balanceservice/service/BalanceService.java
+++ src/main/java/com/maveric/balanceservice/service/BalanceService.java
@@ -1,4 +1,16 @@
 package com.maveric.balanceservice.service;
 
+import com.maveric.balanceservice.model.Balance;
+import com.maveric.balanceservice.repository.BalanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BalanceService {
+    @Autowired
+    BalanceRepository balanceRepository;
+
+    public Balance createBalance(Balance balance){
+       return balanceRepository.save(balance);
+    }
 }
